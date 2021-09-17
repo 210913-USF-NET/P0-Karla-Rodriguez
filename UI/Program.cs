9a5925d683decadb1f300e3ba2e053a7dac623f6
berry-peachy <karla.rodriguez@revature.net>
2021-09-17 18:52:19 -0400

    even more recent actually

--- UI/Program.cs
+++ UI/Program.cs
@@ -7,16 +7,6 @@ namespace UI
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Chocobo Square!");
-            
-            VendorBranches myVendors = new VendorBranches() {
-                Name = "Rowena",
-                GrandCompany = "Twin Adders, Gridania"
-            };
-
-            Console.WriteLine(myVendors.Name);
-            Console.WriteLine(myVendors.GrandCompany);
-            
-        }
+         
     }
 }

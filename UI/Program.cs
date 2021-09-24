e95be58501a0b268b4fa1e8860b07847338d30d8
berry-peachy <karla.rodriguez@revature.net>
2021-09-23 20:39:10 -0400

    got a little turned around, testing now

--- UI/Program.cs
+++ UI/Program.cs
@@ -8,7 +8,15 @@ namespace UI
     {
         static void Main(string[] args)
         {
-            
+        Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .WriteTo.Console()
+        .WriteTo.File("../logs/logs.text", rollingInterval: RollingInterval.Day)
+        .CreateLogger();
+        
+        new MainMenu().Start();
+        Log.Information("Application closing...");
+        Log.CloseAndFlush();
         }
     }
 }
